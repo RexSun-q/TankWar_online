fef56aafb7573e1724326c25bf34bb31930594b5
rex <imsunzhiqi@outlook.com>
2021-04-28 18:44:44 +0800

    去掉初始生成敌人坦克

--- src/rex/tank/Main.java
+++ src/rex/tank/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         TankFrame tankFrame = new TankFrame();
 
-        for (int i = 0; i < 10; i++) {
-            tankFrame.enemyTanks.add(new Tank(30 + 80 * i, 200, Dir.DOWN, Group.BAD, tankFrame));
-        }
+//        for (int i = 0; i < 10; i++) {
+//            tankFrame.enemyTanks.add(new Tank(30 + 80 * i, 200, Dir.DOWN, Group.BAD, tankFrame));
+//        }
         while (true) {
             Thread.sleep(50);
             tankFrame.repaint();
